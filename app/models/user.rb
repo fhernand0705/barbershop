ed1b338bcc8c41918923caef96b08d60a15a6730
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-25 13:24:00 -0500

    added jquery and bootstrap to js file + added validations and associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, :uniqueness => {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 4}
+
+  has_secure_password
+
+  has_many :appointments
+  has_many :shops, through: :appointments
+  has_many :schedules
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
 end
