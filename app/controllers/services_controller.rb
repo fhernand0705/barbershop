474d336ccd363631e5cbf09d29568f15e28c38a0
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-28 00:00:22 -0500

    configured service controller + created form for views/new

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,13 +1,44 @@
 class ServicesController < ApplicationController
+  before_action :set_service, only: [:edit, :show, :update]
+
   def new
+    @service = Service.new
+  end
+
+  def create
+    @service = Service.new(service_params)
+    if @service.valid?
+       @service.save
+       flash[:success] = "#{@service.name} added to list of services!"
+       redirect_to service_path(@service)
+    else
+      render 'services/new'
+    end
+  end
+
+  def show
   end
 
   def edit
   end
 
   def index
+    @service = Service.all
   end
 
-  def show
+  def destroy
+    @service = Service.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+  def set_service
+    @service = Service.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(:name, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :shops
+  resources :services
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -12,10 +13,6 @@ Rails.application.routes.draw do
   # get 'schedules/edit'
   # get 'schedules/show'
   # get 'schedules/index'
-  # get 'services/new'
-  # get 'services/edit'
-  # get 'services/index'
-  # get 'services/show'
   # get 'appointments/new'
   # get 'appointments/edit'
   # get 'appointments/show'
