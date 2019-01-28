21991720b372b5ee5def2a229940de5bac70dd32
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-01-27 22:05:28 -0500

    configured shops controller + added form for shops new file

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,13 +1,49 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: [:edit, :show, :update]
+
   def new
+    @shop = Shop.new
   end
 
-  def edit
+  def create
+    @shop = Shop.new(shop_params)
+    if @shop.valid?
+      @shop.save
+      redirect_to shops_path, :flash => { :success => "#{@shop.name} created!" }
+    else
+      render 'shops/new'   
   end
 
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @shop.update(shop_params)
+    redirect_to shops_path(@shop)
+  end
+
   def index
+    @shop = Shop.all
+  end
+
+  def destroy
+    @shop = User.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
+
+  def shop_params
+    params.require(:shop).premit(:name,
+                                 :address,
+                                 :phone)
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,4 @@
 class Shop < ApplicationRecord
   has_many :appointments
-  has_many :user, through: :appointments
+  has_many :users, through: :appointments
 end
